d038061f6231888f30f2455672f7941d0dbdbe78
kj-9 <46800811+kj-9@users.noreply.github.com>
2023-09-02 14:59:25 +0900

    update for pmtiles

--- front/src/index.js
+++ front/src/index.js
@@ -1,4 +1,4 @@
-import { Map, NavigationControl } from 'maplibre-gl';
+import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import {
 	MaplibreExportControl,
@@ -7,15 +7,30 @@ import {
 	Format,
 	DPI
 } from '@watergis/maplibre-gl-export';
+import * as pmtiles from "pmtiles";
 
-import style from './osmt-kumejima.json' // change style here
+import style from '../../style/osmt-kumejima.json' // change style here
 
-// TODO：pmtilesとして読み込むには、pmtiles://のようなプロトコルを付ける必要がある
-const map = new Map({
+// apply pmtiles source
+const pmtilesSource = {
+    "openmaptiles": {
+        "type": "vector",
+        "url": `pmtiles://${location.protocol}//${location.host}/kyushu-latest.pmtiles` // pmtiles file at public folder
+    }
+}
+style.sources = pmtilesSource;
+
+
+// add pmtiles protocol
+let protocol = new pmtiles.Protocol();
+maplibregl.addProtocol("pmtiles",protocol.tile);
+
+
+const map = new maplibregl.Map({
 	container: 'map',
 	style: style,
 });
-map.addControl(new NavigationControl({}), 'top-right');
+map.addControl(new maplibregl.NavigationControl({}), 'top-right');
 map.addControl(
 	new MaplibreExportControl({
 		PageSize: Size.A3,
