50d8eeaceb48b7a09f4f0b49074fb9f76df44c2b
kj-9 <46800811+kj-9@users.noreply.github.com>
2023-09-02 22:26:13 +0900

    for dev env serve style dir

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,3 +1,14 @@
-export default {
+import { defineConfig, searchForWorkspaceRoot } from 'vite'
+
+export default defineConfig({
     base: 'local-tile-craft', // for github pages: https://ja.vitejs.dev/guide/static-deploy.html#github-pages
-}
+    server: {
+    fs: {
+      allow: [
+        // search up for workspace root
+        searchForWorkspaceRoot(process.cwd()),
+        '../style',
+      ],
+    },
+  },
+})
